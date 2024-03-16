de41f50c9ef1d8e4b772355e29dcfeb66562acdc
jitendravermaa <vermaj502gmail.com>
2024-03-16 23:08:11 +0530

    signup code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,11 @@
  * I need to intercept this
  */
 const authController = require("../controllers/auth.controller")
+const authMW = require("../Middlewares/auth.mw")
+
 
 
 
 module.exports = (app)=>{
-    app.post("/ecomm/api/v1/auth/signup", authController.signup)
+    app.post("/ecomm/api/v1/auth/signup",[authMW.verifySignUpBody], authController.signup)
 }
